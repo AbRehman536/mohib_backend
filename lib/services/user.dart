91eb3e0dc7b71f64845800403695353828b03759
Abdullah <abdullahrehman1019@gmail.com>
2026-02-09 15:29:20 -0600

    Authentication done

---
+++ lib/services/user.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mohib_backend/models/user.dart';
+
+class UserServices{
+  String userCollection = "UserCollection";
+  ///Create User
+  Future createUser(UserModel model)async{
+    return await FirebaseFirestore.instance
+        .collection(userCollection)
+        .doc(model.docId)
+        .set(model.toJson(model.docId.toString()));
+  }
+  ///Delete User
+  Future deleteUser(String userID)async{
+    return await FirebaseFirestore.instance
+        .collection(userCollection)
+        .doc(userID)
+        .delete();
+  }
+  ///Update User
+  Future updateUser(UserModel model)async{
+    return await FirebaseFirestore.instance
+        .collection(userCollection)
+        .doc(model.docId)
+        .update({"name": model.name, "phone": model.phone, "address": model.address});
+  }
+  ///Get User By ID
+  Stream<UserModel> getUserByID(String userID){
+    return FirebaseFirestore.instance
+        .collection(userCollection)
+        .doc(userID)
+        .snapshots()
+        .map((user)=> UserModel.fromJson(user.data()!));
+  }
+}
